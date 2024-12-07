5ba8aeaf87bee0efa7a7d75dbce5dff3b9ed9b99
Melanie Gomez-Bustos <melaniejanettegz@gmail.com>
2024-12-07 14:56:44 -0800

    Implemented getting algorithm info and sending to frontend

--- backend/app.py
+++ backend/app.py
@@ -13,6 +13,8 @@ cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 ship = None
+process = None
+moves = None
 
 @app.route("/fileUploadLoad", methods=["POST", "GET"])
 @cross_origin()
@@ -38,9 +40,16 @@ def fileUploadBalance():
         ship = create_ship(file)
         balance_instance = Balance(ship, file_name)
         balance_instance.balance()
-        print(balance_instance.process)
+        process = balance_instance.process
         return{"Success": 200}
-    return {'message': get_file()}
+    path = []
+    ids = []
+    times = []
+    for move in process:
+        ids.append(move[0])
+        times.append(move[1])
+        path.append(move[2])
+    return {'paths': path, 'ids': ids, 'times': times}
 
 @app.route("/login", methods=["POST", "GET"])
 @cross_origin()
@@ -53,31 +62,37 @@ def login():
         return{"Success":200}
     return {'first_name': get_user()}
 
-@app.route("/load", methods=["POST"])
+@app.route("/load", methods=["POST", "GET"])
 @cross_origin()
 def get_transfer_info():
-    data = request.get_json()
-    load = data.get('load')
-    unload = data.get('unload')
-    print(load)
-    print(unload)
-    ship_grid = [[None for _ in range(12)] for _ in range(8)]
-    # ship = set_file(data)
-    ship_grid = ship.shipgrid
-    load_list = [Cargo(name, None) for name in load]
-    unload_list = []
-    print(ship_grid)
-    for coord in unload:
-        x, y = map(int, coord.strip("[]").split(","))
-        unload_list.append(ship_grid[x - 1][ y - 1])
-    print(unload_list)
-    tm = TransferManager(load_list,unload_list,ship)
-    tm.set_goal_locations()
-    tm.transfer_algorithm()
-    moves = tm.get_paths()
-
-    print(moves)
-    return{'paths': moves}
+    if(request.method == "POST"):
+        data = request.get_json()
+        load = data.get('load')
+        unload = data.get('unload')
+        print(load)
+        print(unload)
+        ship_grid = [[None for _ in range(12)] for _ in range(8)]
+        ship_grid = ship.shipgrid
+        load_list = [Cargo(name, None) for name in load]
+        unload_list = []
+        print(ship_grid)
+        for coord in unload:
+            x, y = map(int, coord.strip("[]").split(","))
+            unload_list.append(ship_grid[x - 1][ y - 1])
+        print(unload_list)
+        tm = TransferManager(load_list,unload_list,ship)
+        tm.set_goal_locations()
+        tm.transfer_algorithm()
+        moves = tm.get_paths()
+        return{"Success":200}
+    path = []
+    ids = []
+    times = []
+    for move in moves:
+        ids.append(move[0])
+        path.append(move[1])
+        times.append(move[2])
+    return{'paths': path, 'ids': ids, 'times': times}
     
 # You will get a list of container ids to unload/load
 # Use that and create the load and unload lists and run the algorithm, algorithm should have the steps in lists, which you can get using .get_unload_paths and .get_load_paths
@@ -89,12 +104,6 @@ def get_transfer_info():
 #   ids: should match the above indices -> list
 #   times: should match the above indices -> list
 # }    
-
-@app.route("/balance", methods=["POST"])
-@cross_origin()
-def balance():
-    
-    return{'Success':200}
 
 if __name__ == '__main__':
     app.run(debug=True)
